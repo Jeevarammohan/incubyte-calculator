e636644e2f0f9da3540ea18fd8962adc6f94c64f
Jeeva R <jeeva.r@acheron-tech.com>
2024-12-18 00:28:59 +0530

    adding GREEN of TDD ie. code to pass the failing tesetcase

--- calculator/src/main/java/com/assignment/calculator/controller/CalculatorController.java
+++ calculator/src/main/java/com/assignment/calculator/controller/CalculatorController.java
@@ -2,6 +2,7 @@ package com.assignment.calculator.controller;
 
 import com.assignment.calculator.service.CalculatorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,8 +25,15 @@ public class CalculatorController {
 
     @PostMapping("/add")
     public ResponseEntity<String> addNumbers(@RequestBody String numbers){
-        int sum = calculatorService.addNumber(numbers);
-        return ResponseEntity.ok("Sum is: "+sum );
+        try {
+            // Calling the service to add the numbers and get the sum
+            int sum = calculatorService.addNumber(numbers);
+            return ResponseEntity.ok("Sum is: " + sum);
+        } catch (IllegalArgumentException e) {
+            // If negative numbers are found, returning a bad request with an appropriate error message
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(e.getMessage());
+        }
     }
 
 }
